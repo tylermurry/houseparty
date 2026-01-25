24c71770f9909ba894ec4e5fcdbe8c5b56656ceb
Tyler Murry <tyman7@gmail.com>
2026-01-25 14:41:44 -0600

    Initial files from Aspire .NET + React starter

---
+++ HouseParty.AppHost/AppHost.cs
@@ -0,0 +1,17 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var cache = builder.AddRedis("cache");
+
+var server = builder.AddProject<Projects.HouseParty_Server>("server")
+    .WithReference(cache)
+    .WaitFor(cache)
+    .WithHttpHealthCheck("/health")
+    .WithExternalHttpEndpoints();
+
+var webfrontend = builder.AddViteApp("webfrontend", "../frontend")
+    .WithReference(server)
+    .WaitFor(server);
+
+server.PublishWithContainerFiles(webfrontend, "wwwroot");
+
+builder.Build().Run();
